be80ac366c392310f3bc326c0214cb404117ec6d
Mason3144 <lts890303@gmail.com>
2022-05-25 06:51:45 +0900

    Real-time chat with Websockets, ws

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,46 @@
-alert("hey")
+
+
+const messageList = document.querySelector("ul")
+const nickForm = document.querySelector("#nick")
+const messageForm = document.querySelector("#chat")
+const socket = new WebSocket(`ws://${window.location.host}`)
+
+const makeMessage = (type, payload) => {
+    const msg = { type, payload }
+    return JSON.stringify(msg)
+    //     object를 보낼때
+    //     보내기전 JSON.stringify()로 string으로 보낸후
+    //     받을때 JSON.parse()로 다시 오브젝트로 변환
+}
+
+socket.addEventListener("open", () => {
+    console.log("connected")
+})
+socket.addEventListener("message", (message) => {
+    const li = document.createElement("li")
+    li.innerText = message.data
+    messageList.appendChild(li)
+})
+socket.addEventListener("close", () => {
+    console.log("disconnected")
+})
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value))
+    input.value = ""
+}
+
+const handleNickSubmit = (event) => {
+    event.preventDefault();
+    let inputValue = []
+    const input = nickForm.querySelector("input")
+    input.placeholder = `Your nickname is "${input.value}"`
+    inputValue = input.value
+    input.value = ""
+    socket.send(makeMessage("nickname", inputValue));
+}
+
+messageForm.addEventListener("submit", handleSubmit)
+nickForm.addEventListener("submit", handleNickSubmit)
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express"
 
 const app = express();
@@ -5,11 +7,45 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views")
 app.use("/public", express.static(__dirname + "/public"))
-
 app.get("/", (req, res) => res.render("home"))
 app.get("/*", (req, res) => res.redirect("/"));
 
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`)
-app.listen(3000, handleListen);
+
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
+
+const sockets = [];
+
+
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "Anonymous";
+    console.log("connected")
+    socket.on("close", () => console.log("disconnected"))
+    socket.on("message", msg => {
+        const message = JSON.parse(msg)
+        // if (message.type === "new_message") {
+        //     sockets.forEach(aSocket => aSocket.send(message.payload))
+        // } else if (message.type === "nickname") {
+        //     console.log(message.payload)
+        // } object를 if문이 아닌 switch로 사용
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`))
+                break                   //return과 같은역활,안해줄경우 다음case까지도 계속 진행됨
+            case "nickname":
+                socket["nickname"] = message.payload   //object에 요소 추가하는 방식, 
+                break                                   //socket.nickname = massage.payload
+        }
+    })
+
+})
+
+
+
+server.listen(3000, handleListen);
 
 
