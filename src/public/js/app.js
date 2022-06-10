28f20a08e501867bf95a8366f377f31b362762fe
Mason3144 <lts890303@gmail.com>
2022-06-10 16:27:15 +0900

    RTC peer connection with rooms, offers, answers, icecandidate, senders and STUN server.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,11 +4,16 @@ const myFace = document.getElementById("myFace")
 const muteBtn = document.getElementById("mute")
 const cameraBtn = document.getElementById("camera")
 const cameraSelect = document.getElementById("cameras")
+const call = document.getElementById("call")
+
+call.hidden = true
 
 let myStream;
 let muted = false
 let blind = false
-
+let roomName;
+let myPeerConnection;
+let dataChannel
 
 const getCameras = async () => {
     try {
@@ -54,10 +59,8 @@ const getMedia = async (deviceId) => {
     }
 }
 
-getMedia();
 
 const handleMute = () => {
-    console.log(myStream.getAudioTracks())
     myStream.getAudioTracks().forEach((track) => track.enabled = !track.enabled)
     // myStream.getAudioTracks()가 array이기때문에 forEach를 사용
     //video.muted 는 비디오 재생에서 소리를끄는것이며 getAudioTracks는 마이크를 제어
@@ -82,8 +85,109 @@ const handleBlind = () => {
 
 const handleCameraChange = async () => {
     await getMedia(cameraSelect.value)
+    if (myPeerConnection) {
+        const videoTrack = myStream.getVideoTracks()[0]
+        const videoSender = myPeerConnection.getSenders().find((sender) => sender.track.kind === "video");
+        videoSender.replaceTrack(videoTrack)
+    }
 }
 
 muteBtn.addEventListener("click", handleMute)
 cameraBtn.addEventListener("click", handleBlind)
 cameraSelect.addEventListener("input", handleCameraChange)
+
+
+//welcome form (getting inside of a room)
+
+const welcome = document.getElementById("welcome")
+const welcomeForm = welcome.querySelector("form")
+
+const initCall = async () => {
+    welcome.hidden = true
+    call.hidden = false
+    await getMedia()
+    makeConnection()
+}
+
+
+const handleWelcomeSubmit = async (event) => {
+    event.preventDefault()
+    const input = welcomeForm.querySelector("input")
+    await initCall()
+    socket.emit("join_room", input.value)
+    roomName = input.value
+    input.value = ""
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit)
+
+//socket
+
+socket.on("welcome", async () => {
+    dataChannel = myPeerConnection.createDataChannel("chat");
+    dataChannel.addEventListener("message", console.log)
+
+
+
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer)
+    socket.emit("offer", offer, roomName);
+})
+
+socket.on("offer", async (offer) => {
+    myPeerConnection.addEventListener("datachannel", (event) => {
+        dataChannel = event.channel;
+        dataChannel.addEventListener("message", console.log)
+    })
+
+
+
+    myPeerConnection.setRemoteDescription(offer)
+    const answer = await myPeerConnection.createAnswer()
+    myPeerConnection.setLocalDescription(answer)
+    socket.emit("answer", answer, roomName);
+})
+
+socket.on("answer", (answer) => {
+    myPeerConnection.setRemoteDescription(answer)
+})
+
+socket.on("ice", (ice) => {
+    myPeerConnection.addIceCandidate(ice)
+})
+
+//RTC code
+
+
+
+
+const makeConnection = () => {
+    myPeerConnection = new RTCPeerConnection({
+        iceServers: [{
+            urls: ["stun:ntk-turn-2.xirsys.com"]
+        }, {
+            username: "bcdsuD0lobDXBFNpGsNIULgJDNNpzEwi9Gz-TfM0FfqvJAQFQFbdQMoLAY9QR62hAAAAAGKiuIx4MTIxMjEy",
+            credential: "50a7b9c4-e86c-11ec-9500-0242ac120004",
+            urls: [
+                "turn:ntk-turn-2.xirsys.com:80?transport=udp",
+                "turn:ntk-turn-2.xirsys.com:3478?transport=udp",
+                "turn:ntk-turn-2.xirsys.com:80?transport=tcp",
+                "turn:ntk-turn-2.xirsys.com:3478?transport=tcp",
+                "turns:ntk-turn-2.xirsys.com:443?transport=tcp",
+                "turns:ntk-turn-2.xirsys.com:5349?transport=tcp"
+            ]
+        }]
+    });// stun server offered by google, you need to make own stun server instead of google if you make a real peer to peer comunication service
+    myPeerConnection.addEventListener("icecandidate", handleIce)
+    myPeerConnection.addEventListener("track", handleAddStream)
+    myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream))
+}
+
+const handleIce = (data) => {
+    socket.emit("ice", data.candidate, roomName)
+}
+
+const handleAddStream = (data) => {
+    const peerStream = document.getElementById("peerStream")
+    peerStream.srcObject = data.streams[0]
+}
